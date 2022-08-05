0c0562313fad2a8be0a6eaec84401cb6998dace4
omar saade <omarsaadeh11@gmail.com>
2022-08-05 16:35:25 +0300

    react

--- workingwithformreact/src/components/SimpleInput.js
+++ workingwithformreact/src/components/SimpleInput.js
@@ -1,9 +1,59 @@
+import React, { useRef, useState, useEffect } from 'react'
+
+
 const SimpleInput = (props) => {
+
+
+
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+
+
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log('Name Input is Valid');
+    }
+  }, [enteredNameIsValid]);
+
+
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === '') { setEnteredNameIsValid(false); return; }
+
+    setEnteredNameIsValid(true);
+    const enteredValue = nameInputRef.current.value;
+    setEnteredName('');
+  }
+
+
+
+
+  const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched;
+  const nameInputClasses = nameInputIsInValid ? 'form-control invalid' : 'form-control';
+
+  // console.log(nameInputClasses); //form-control
+  // console.log(nameInputIsInValid);//false
+
+
   return (
-    <form>
-      <div className='form-control'>
+
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} value={enteredName} type='text' id='name' onChange={nameInputChangeHandler} />
+        {nameInputIsInValid && <p className='error-text'>Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
@@ -12,4 +62,46 @@ const SimpleInput = (props) => {
   );
 };
 
+
+
 export default SimpleInput;
+
+
+// initillay
+
+// enteredNameIsValid false
+// enteredNameTouched   false
+// const nameInputIsInValid false
+// const nameInputClasses: form-control
+
+// ==================
+
+// fadye
+
+// enteredNameTouched   true
+// enterdnameisvalid false
+// nameInputisinvalid : true
+// nameInputclasses : form-control invalid
+// <p>sss</p>
+
+// ==============================
+// fiii
+
+
+// enteredNametouched true 
+// enterednameisvalid true
+// nameInputisinvalid = false
+// nameInputclasses = form-control
+
+// ============================
+
+
+
+
+
+
+
+
+
+
+
